ca63190b85f4a89d1a605bca8dffe99f6a02ef2d
teimaa2004-byte <teimaa2004@icloud.com>
2026-05-14 12:30:29 +0200

    Add files via upload

---
+++ web/header.php
@@ -0,0 +1,9 @@
+<header class="main-header">
+    <h1>🏆 Club Info_Tech - Gestion des Concours</h1>
+</header>
+<nav>
+    <a href="index.php">📋 Liste Concours</a>
+    <a href="resultats.php">🏅 Résultats</a>
+    <a href="inscription.php">✍️ Inscription</a>
+    <a href="requetes.php">💻 Requêtes Libres</a>
+</nav>
